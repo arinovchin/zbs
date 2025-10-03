f5150089f680cf4b23fc1388d73ea24d9246ac50
Ovchinnikova Arina Andreevna <ariovch52@gmail.com>
2025-10-04 00:06:16 +0700

    Решён конфликт rebase

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #define SIZE 1024
 int main(int argc, char** argv) {
 char* str = (char*)malloc(sizeof(char) * SIZE);
-printf("Введите своё имя:\n");
-str = gets(str);
-printf("Привет, %s", str);
+printf("Введите своё имя: \n");
+str = gets(str);;
+puts("Привет");
+puts(str);
 return 0;
 }
